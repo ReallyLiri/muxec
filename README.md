775f7d6ed00798d0593d98f2e6e57febd0b30d3d
ReallyLiri <reallyliri@gmail.com>
2021-11-28 20:06:04 +0200

    working on setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# Multiplexed Executor
+
+TBD
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+__version__ = '0.1.0'
+
+with open("README.md", "r") as f:
+    long_description = f.read()
+
+setup(
+    name="muxec",
+    description="Multiplexed Exec Tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    version=__version__,
+    author='ReallyLiri',
+    url='https://github.com/ReallyLiri/muxec',
+    author_email='reallyliri@gmail.com',
+    keywords='mux multiplex exec execute xargs shell parallel',
+    py_modules=['muxec'],
+    python_requires='>=3.6',
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
